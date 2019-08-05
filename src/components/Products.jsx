8a17e25044dc1cccb5eeda29e1549927b56ead33
tivler <thomasdmckyer@gmail.com>
2019-08-05 10:38:22 -0400

    setting up JSON Object:

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
-function Products () {
-    return (
-        <>
-        <h1>Tivler</h1>
-        </>
-    )
+class Products extends React.Component {
+
+    render () {
+        return (
+            <>
+            <h1>Tivler</h1>
+            </>
+        )
+    }
 }
 
 export default Products
