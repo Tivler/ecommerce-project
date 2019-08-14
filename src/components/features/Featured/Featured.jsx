dc68bd69e7ed70c515286bf631339f74093c2010
tivler <thomasdmckyer@gmail.com>
2019-08-14 16:09:40 -0400

    fix

--- src/components/features/Featured/Featured.jsx
+++ src/components/features/Featured/Featured.jsx
@@ -11,11 +11,11 @@ class Featured extends React.Component {
         });
 
         return (
-        <div className="album">
-            <h2 className="album__title">Featured Albums</h2>
-            <p className="album__description">Slotify has all of your favorite albums right at your fingers</p>
+        <div className="featured">
+            <h2 className="featured__title">Featured Albums</h2>
+            <p className="featured__description">Slotify has all of your favorite albums right at your fingers</p>
 
-            <div className="album__wrapper">
+            <div className="featured__wrapper">
                 {featuredList}
             </div>
         </div>
--- src/featured.js
+++ src/featured.js
@@ -1,10 +1,10 @@
 const featured = [
-    { id: 'one', title: "Take Care", artist: "Drake", price: 9.99},
-    { id: 'two', title: "PTSD", artist: "Rod Wave", price: 9.99},
-    { id: 'three', title: "My Moment", artist: "Tee Grizzley", price: 11.99},
-    { id: 'four', title: "True To Self", artist: "Bryson Tiller", price: 10.99},
-    { id: 'five', title: "Feed Tha Streets", artist: "Roddy Ricch", price: 7.99},
-    { id: 'six', title: "Life B4 Fame", artist: "Quando Rondo", price: 8.99}
+    { id: 'One', title: "Take Care", artist: "Drake", price: 9.99},
+    { id: 'Two', title: "PTSD", artist: "Rod Wave", price: 9.99},
+    { id: 'Three', title: "My Moment", artist: "Tee Grizzley", price: 11.99},
+    { id: 'Four', title: "True To Self", artist: "Bryson Tiller", price: 10.99},
+    { id: 'Five', title: "Feed Tha Streets", artist: "Roddy Ricch", price: 7.99},
+    { id: 'Six', title: "Life B4 Fame", artist: "Quando Rondo", price: 8.99}
 ]
 
 export default featured
