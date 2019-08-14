583dee72acb7bbb84639119e408ea013f38aecef
tivler <thomasdmckyer@gmail.com>
2019-08-14 12:18:07 -0400

    updated footer component

--- src/components/core/Footer/Footer.jsx
+++ src/components/core/Footer/Footer.jsx
@@ -16,16 +16,16 @@ const Footer = () => {
         <footer className="footer">
             <h2 className="footer__heading">Get In Touch</h2>
             <div className="footer__socials">
-                <a className="footer__socials__icon" href="https://www.linkedin.com/in/thomas-mckyer/"><FontAwesomeIcon className="footer__socials__icon--size" icon={['fab' , 'linkedin']} /><span className="label">LinkedIn</span></a>
-                <a className="footer__socials__icon divider" href="https://www.instagram.com/tmckyer/"><FontAwesomeIcon className="footer__socials__icon--size" icon={['fab' , 'instagram']} /><span className="label">Instagram</span></a>
-                <a className="footer__socials__icon" href="https://github.com/tivler"><FontAwesomeIcon className="footer__socials__icon--size" icon={['fab' , 'github-square']} /><span className="label">Github</span></a>
+                <a className="footer__socials__icon" href="https://www.linkedin.com/in/thomas-mckyer/"><FontAwesomeIcon className="footer__socials__icon--size" icon={['fab' , 'linkedin']} /><span className="footer__socials__label">LinkedIn</span></a>
+                <a className="footer__socials__icon divider" href="https://www.instagram.com/tmckyer/"><FontAwesomeIcon className="footer__socials__icon--size" icon={['fab' , 'instagram']} /><span className="footer__socials__label">Instagram</span></a>
+                <a className="footer__socials__icon" href="https://github.com/tivler"><FontAwesomeIcon className="footer__socials__icon--size" icon={['fab' , 'github-square']} /><span className="footer__socials__label">Github</span></a>
             </div>
 
             <nav className="footer__nav">
                 <div className="footer__info">
-                    <a className="footer__info__icon" href="https://www.linkedin.com/in/thomas-mckyer/"><FontAwesomeIcon className="footer__info__icon--size" icon={faPhoneAlt} /><span className="label">(704) 808 8000</span></a>
-                    <a className="footer__info__icon divider" href="https://www.instagram.com/tmckyer/"><FontAwesomeIcon className="footer__info__icon--size" icon={faEnvelope} /><span className="label">information@slotify.io</span></a>
-                    <a className="footer__info__icon" href="https://github.com/tivler"><FontAwesomeIcon className="footer__info__icon--size" icon={faMapMarkerAlt} /><span className="label">123 Somewhere Lit Drive, Charlotte, NC 28277</span></a>
+                    <a className="footer__info__icon" href="https://www.linkedin.com/in/thomas-mckyer/"><FontAwesomeIcon className="footer__info__icon--size" icon={faPhoneAlt} /><span className="footer__info__label">(704) 808 8000</span></a>
+                    <a className="footer__info__icon divider" href="https://www.instagram.com/tmckyer/"><FontAwesomeIcon className="footer__info__icon--size" icon={faEnvelope} /><span className="footer__info__label">information@slotify.io</span></a>
+                    <a className="footer__info__icon" href="https://github.com/tivler"><FontAwesomeIcon className="footer__info__icon--size" icon={faMapMarkerAlt} /><span className="footer__info__label">123 Somewhere Lit Drive, Charlotte, NC 28277</span></a>
                 </div>
             </nav>
 
