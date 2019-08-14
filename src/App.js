58b51f8901117fecff648217eeec9994dd6af87b
tivler <thomasdmckyer@gmail.com>
2019-08-14 16:48:58 -0400

    commented out imports

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Slider from './components/features/Slider/Slider';
 import Featured from './components/features/Featured/Featured';
 import Footer from './components/core/Footer/Footer';
 import Quotes from './components/features/Quotes/Quotes';
-import Products from './components/features/Products/Products';
-import Form from './components/features/Form/Form';
+// import Products from './components/features/Products/Products';
+// import Form from './components/features/Form/Form';
 
 class App extends React.Component {
   constructor(props) {
@@ -22,8 +22,9 @@ class App extends React.Component {
     return (
       <div className="App">
         <Navbar />
-        
-         <Form />
+        <Slider />
+        <Featured albums={this.state.featured} />
+        <Quotes />
         <Footer />
       </div>
     );
