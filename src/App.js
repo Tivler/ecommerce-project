1d7acf795bbd7a8eec02d22e4736dd153fa740fe
tivler <thomasdmckyer@gmail.com>
2019-08-14 20:55:00 -0400

    updated grid

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import './App.css';
+
 import albums from './albums';
 import featured from  './featured';
 
--- src/components/feature/Quotes/Quotes.jsx
+++ src/components/feature/Quotes/Quotes.jsx
@@ -6,7 +6,7 @@ const Quotes = () => {
     <> 
     <section className="quote">
         <div className="quote--wrapper">
-            <h2 className="quote__title">Find Your Sound</h2>
+            <h2 className="quote__title">Find A Sound That Fits You</h2>
             <p className="quote__content">"There is something here for everyone! Find your sound, create your own wave find music and a sound that speaks to you. That's why we are here that is what Slotify is all about."</p>
             <p className="quote__author">Thomas McKyer</p>
             <p className="quote__author--title">Founder, Slotify</p>
