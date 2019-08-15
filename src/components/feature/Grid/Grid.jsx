6a81a81c65b74b512cde6674de11ade1f55b09ca
tivler <thomasdmckyer@gmail.com>
2019-08-15 18:39:15 -0400

    added filter to products page

--- src/components/feature/Grid/Grid.jsx
+++ src/components/feature/Grid/Grid.jsx
@@ -1,19 +1,45 @@
 import React from 'react';
 
-import Albums from '../../Albums';
+import albums from '../../../albums';
+
+import AlbumCard from '../../AlbumCard';
 import GridHead from './GridHead';
 
 import './_Grid.scss';
 import './_Filter.scss';
 
 class Grid extends React.Component {
-    render () {
-        const products = this.props.albums.map((album,index) => {
-            return (
-                <Albums key={index} albums={album} />
-            )
-        })
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            products: []
+        };
+    }
+
+    _filterByLowest = () => {
+        const albums = this.state.products;
+        const albumsByLowest = albums.sort((x, y) => {
+            return x.price - y.price;
+        });
+        this.setState({ products: albumsByLowest })
+        console.log('filtered by lowest:', albumsByLowest);
+    }
+
+    _filterByHighest = () => {
+        const albums = this.state.products;
+        const albumsByHighest = albums.sort((x, y) => {
+            return y.price - x.price;
+        });
+        this.setState({ products: albumsByHighest })
+        console.log('filtered by Highest:', albumsByHighest);
+    }
+
+    componentDidMount() {
+        this.setState({ products: albums });
+    }
+
+    render () {
         return (
 
         <>
@@ -24,12 +50,13 @@ class Grid extends React.Component {
             <p className="album__description">All the sounds, All the waves find something that fits you</p>
 
             <div className="album__wrapper">
-                {products}
+                {this.state.products.map(p => <AlbumCard id={p.id} key={p.id} url={p.url} title={p.title} artist={p.artist} price={p.price} />)}
             </div>
+
             <div className="filter">
                 <h3 className="filter__title">Filter Albums By Price:</h3>
-                <button className="filter__button filter__button--divider">High To Low</button>
-                <button className="filter__button">Low To High</button>
+                <button onClick={this._filterByHighest}className="filter__button filter__button--divider">High To Low</button>
+                <button onClick={this._filterByLowest} className="filter__button">Low To High</button>
             </div>
         </div>
         </>
